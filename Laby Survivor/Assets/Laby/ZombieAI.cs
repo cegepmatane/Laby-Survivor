97be55be69af7d254fad121080719d16dbfb4e55
zhisheng <zhishengtrieu@gmail.com>
2023-02-14 15:12:33 -0500

    Modification du script pour le mouvement du zombie

--- Laby Survivor/Assets/Laby/ZombieAI.cs
+++ Laby Survivor/Assets/Laby/ZombieAI.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-public class NavAI : MonoBehaviour{
+public class ZombieAI : MonoBehaviour{
     public GameObject player; 
     NavMeshAgent agent;
 
@@ -17,34 +17,39 @@ public class NavAI : MonoBehaviour{
             agent.SetDestination(player.transform.position);
             if (distance > 12){
                 //on marche vers le joueur
-                agent.speed = 3;
-                agent.acceleration = 5;
-                agent.angularSpeed = 110;
+                agent.speed = 0.7f;
+                agent.acceleration = 10;
+                agent.angularSpeed = 250;
                 //on change l'animation
                 GetComponent<Animator>().Play("Z_Walk");
-            }else if( distance > 1.3f){
+            }else if( distance > 1.2f){
                 //si la distance entre le joueur et l'agent est inférieure à 12 et supérieure à 1.1
                 //on court vers le joueur
-                agent.speed = 4;
-                agent.acceleration = 7;
-                agent.angularSpeed = 120;
+                agent.speed = 1.5f;
+                agent.acceleration = 2;
+                agent.angularSpeed = 250;
                 //on change l'animation
                 GetComponent<Animator>().Play("Z_Run");
             }else{
                 //on arrete le mouvement de l'agent
                 agent.SetDestination(transform.position);
                 //on attaque le joueur 
-                agent.speed = 0;
-                agent.acceleration = 0;
-                agent.angularSpeed = 0;
                 //on change l'animation
                 GetComponent<Animator>().Play("Z_Attack");
                 //on regle la vitesse d'attaque
+                agent.speed = 0;
+                agent.acceleration = 0;
+                agent.angularSpeed = 0;
                 GetComponent<Animator>().speed = 0.7f;
             }
         }else{
+            //on arrete le mouvement de l'agent
+            agent.SetDestination(transform.position);
             GetComponent<Animator>().Play("Z_Idle");
         }
+
+        //si le zombie meurt
+            //GetComponent<Animator>().Play("Z_FallingBack");
     }
  
 }
