83d3a57d857bd8ffd3ad6380d0685c417e68da3a
zhisheng <zhaotrieu1+git@gmail.com>
2023-02-14 10:56:07 -0500

    modification du deplacement du zombie

--- Laby Survivor/Assets/Laby/NavAI.cs
+++ Laby Survivor/Assets/Laby/NavAI.cs
@@ -10,34 +10,41 @@ public class NavAI : MonoBehaviour{
     }
 
     void Update(){
-        //on donne la position du joueur à l'agent mais avec un decalage de 1
-        agent.SetDestination(player.transform.position /*+ new Vector3(1,1,1)*/);
+        //on repere le joueur si il est dans le champ de vision de l'agent qui sera de 25 
         float distance = Vector3.Distance(player.transform.position, transform.position);
-        if (distance > 12){
-            //on marche vers le joueur
-            agent.speed = 3;
-            agent.acceleration = 5;
-            agent.angularSpeed = 110;
-            //on change l'animation
-            GetComponent<Animator>().Play("Z_walk");
-        }else if( distance > 1.1f){
-            //si la distance entre le joueur et l'agent est inférieure à 12 et supérieure à 1.1
-            //on court vers le joueur
-            agent.speed = 4;
-            agent.acceleration = 7;
-            agent.angularSpeed = 120;
-            //on change l'animation
-            GetComponent<Animator>().Play("Z_Run");
+        if (distance <= 20){
+            //on donne la position du joueur à l'agent
+            agent.SetDestination(player.transform.position);
+            if (distance > 12){
+                //on marche vers le joueur
+                agent.speed = 3;
+                agent.acceleration = 5;
+                agent.angularSpeed = 110;
+                //on change l'animation
+                GetComponent<Animator>().Play("Z_Walk");
+            }else if( distance > 1.3f){
+                //si la distance entre le joueur et l'agent est inférieure à 12 et supérieure à 1.1
+                //on court vers le joueur
+                agent.speed = 4;
+                agent.acceleration = 7;
+                agent.angularSpeed = 120;
+                //on change l'animation
+                GetComponent<Animator>().Play("Z_Run");
+            }else{
+                //on arrete le mouvement de l'agent
+                agent.SetDestination(transform.position);
+                //on attaque le joueur 
+                agent.speed = 0;
+                agent.acceleration = 0;
+                agent.angularSpeed = 0;
+                //on change l'animation
+                GetComponent<Animator>().Play("Z_Attack");
+                //on regle la vitesse d'attaque
+                GetComponent<Animator>().speed = 0.7f;
+            }
         }else{
-            //on attaque le joueur 
-            agent.speed = 0;
-            agent.acceleration = 0;
-            agent.angularSpeed = 0;
-            //on change l'animation
-            GetComponent<Animator>().Play("Z_Attack");
-            //on regle la vitesse d'attaque
-            GetComponent<Animator>().speed = 0.7f;
+            GetComponent<Animator>().Play("Z_Idle");
         }
-    }  
+    }
  
 }
