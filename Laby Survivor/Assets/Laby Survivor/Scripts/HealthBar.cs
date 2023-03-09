e5d8bf9c5cdafd2057a030ac690e26e2d7bdff1a
Oxsw1ng <100700178+Oxsw1ng@users.noreply.github.com>
2023-03-09 09:59:20 -0500

    Add files via upload

--- Laby Survivor/Assets/Laby Survivor/Scripts/HealthBar.cs
+++ Laby Survivor/Assets/Laby Survivor/Scripts/HealthBar.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.InputSystem;
 
@@ -10,29 +11,29 @@ public class HealthBar : MonoBehaviour
     GameObject _obj;
     public Player player;
 
+    bool hpDisplayActive;
+    public Image barFill;
+
     public InputActionReference triggerAction;
 
     // Start is called before the first frame update
     void Start()
     {
-        //_obj = gameObject;
-        //_obj.SetActive(false);
-        Debug.Log("Healthbar lancée");
+        _obj = gameObject;
+        _obj.SetActive(false);
+        hpDisplayActive = false;
         triggerAction.action.performed += displayHP;
     }
 
     void displayHP(InputAction.CallbackContext context) {
-        Debug.Log("Test affichage HP");
+        barFill.fillAmount = player.health / player.getMaxHP();
+        hpDisplayActive = !hpDisplayActive;
+        _obj.SetActive(hpDisplayActive);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //bool pressed;
-        /**leftHand.IsPressed(button, out pressed);
- 
-        if (pressed) {
-            Debug.Log("Hello - " + button);
-        }**/
+
     }
 }
