b91a989374a2b2a2b3b2876d046b788f70ba6e3a
Benjamin Mouëza <70459262+Moueben99@users.noreply.github.com>
2023-03-09 09:46:09 -0500

    modification script player pour la récolte des objets

---
+++ Laby Survivor/Assets/Laby Survivor/Scripts/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Perso{
+
+    public List<string> inventaire;
+
+    public void start()
+    {
+        inventaire = new List<string>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Collecte"))
+        {
+            string itemType = collision.gameObject.GetComponent<RecolteObjets>().itemType;
+            print("Vous avez recolte un item:" + itemType);
+
+            inventaire.Add(itemType);
+            print("Inventaire:" + inventaire.Count);
+            //items.Add(itemType);
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Laby Survivor/Assets/Laby/Script/PersoTest.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersoTest : MonoBehaviour{
+    public float health = 50;
+    public float atk = 10;
+    public bool canBeAttacked = true;
+    public int secondsBeforeAttack = 2;
+    // Start is called before the first frame update
+    void Start(){
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+
+    public virtual void TakeDamage(float damage){
+        if (canBeAttacked){
+            health -= damage;
+            if (health < 0){
+                health = 0;
+            }
+            waitBeforeAttack();
+        }
+    }
+
+    /*
+    public virtual void OnCollisionEnter(Collision collision){
+        GameObject perso = collision.gameObject;
+        if (perso.tag == "Player"){
+            Perso instance = perso.GetComponent<Perso>();
+            if (instance.canBeAttacked){
+                Debug.Log("Player is attacked");
+                instance.TakeDamage(atk);
+            }
+        }else if (perso.tag == "Monstre"){
+            //on detruit le monstre
+            Destroy(perso);
+            Debug.Log("Monstre is dead");
+        }
+    }*/
+
+    public void waitBeforeAttack(){
+        canBeAttacked = false;
+        StartCoroutine(attack());
+    }
+
+    IEnumerator attack(){
+        yield return new WaitForSeconds(secondsBeforeAttack);
+        canBeAttacked = true;
+    }
+
+}
