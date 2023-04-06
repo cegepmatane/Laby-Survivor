2000f948af18b59c7b560d32a01ddd4609d42762
Yann Mijatovic <yannmijatovicp@gmail.com>
2023-04-06 16:36:49 +0200

    fruits

--- Laby Survivor/Assets/Laby Survivor/Scripts/RecolteObjets.cs
+++ Laby Survivor/Assets/Laby Survivor/Scripts/RecolteObjets.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class RecolteObjets : MonoBehaviour
 {
     public string itemType;
+
     private void OnTriggerEnter(Collider collision)
     {
-        var ennemie = collision.gameObject.GetComponent<Ennemie>();
-
-        if (ennemie)
-        {
+        if (collision.gameObject.layer == 7) {
+            if (collision.gameObject.GetComponent<Perso>().health <= collision.gameObject.GetComponent<Perso>().getMaxHP() - 10) {
+                collision.gameObject.GetComponent<Perso>().health = collision.gameObject.GetComponent<Perso>().health + 10;
+                Destroy(gameObject);
+            } else {
+               if (collision.gameObject.GetComponent<Perso>().health < collision.gameObject.GetComponent<Perso>().getMaxHP()) {
+                    collision.gameObject.GetComponent<Perso>().health = collision.gameObject.GetComponent<Perso>().getMaxHP();
+                    Destroy(gameObject);
+               }
+            }
             
         }
-        else
-        {
-            Destroy(gameObject);
-        }
+
     }
 }
